66c0f4cc46be650b971bd246a4f7b370e9baef5a
wenerd569 <wenerd569@gmail.com>
2025-02-17 23:15:01 +0300

    It doesn't work but I write almost all

--- src/person.cpp
+++ src/person.cpp
@@ -3,17 +3,3 @@
 Person::Person(long id, string name, Coordinates coordinates, year_month_day creationDate, double height, optional<Color> eyeColor, Color hairColor, Country nationality, optional<Location> location){
     
 }
-Person::Person(string name, Coordinates coordinates, double height, optional<Color> eyeColor, Color hairColor, Country nationality, optional<Location> location){
-    id = ++last_id;
-    
-    auto today = floor<days>(system_clock::now());
-    this->creationDate = today;
-
-    this->name = name;
-    this->coordinates = coordinates;
-    this->height = height;
-    this->eyeColor = eyeColor;
-    this->hairColor = hairColor;
-    this->nationality = nationality;
-    this->location = location;
-}
